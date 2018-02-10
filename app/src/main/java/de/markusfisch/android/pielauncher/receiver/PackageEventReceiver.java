b42bf432a4195898dbe58a04b95fb3f428ac8eb8
Markus Fisch <mf@markusfisch.de>
2018-02-10 13:37:50 +0100

    Check action of package event broadcast

--- app/src/main/java/de/markusfisch/android/pielauncher/receiver/PackageEventReceiver.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/receiver/PackageEventReceiver.java
@@ -7,5 +7,11 @@ import android.content.Intent;
 public class PackageEventReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if (Intent.ACTION_PACKAGE_ADDED.equals(action)) {
+		} else if (Intent.ACTION_PACKAGE_REMOVED.equals(action)) {
+		} else if (Intent.ACTION_PACKAGE_CHANGED.equals(action)) {
+		} else if (Intent.ACTION_PACKAGE_REPLACED.equals(action)) {
+		}
 	}
 }
