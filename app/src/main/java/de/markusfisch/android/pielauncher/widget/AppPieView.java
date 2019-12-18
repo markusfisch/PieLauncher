70cd15fac857843a761cca0055c8397d84cb81f9
Markus Fisch <mf@markusfisch.de>
2019-12-18 22:03:34 +0100

    Revert "Always use max radius for padding"
    Got confused - this is unnecessary of course.
    This reverts commit 2583e3858e925dad4e2791cf9ae99e44374a98ff.

--- app/src/main/java/de/markusfisch/android/pielauncher/widget/AppPieView.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/widget/AppPieView.java
@@ -361,8 +361,8 @@ public class AppPieView extends SurfaceView {
 
 	private void setCenter(int x, int y) {
 		appMenu.set(
-				Math.max(maxRadius, Math.min(viewWidth - maxRadius, x)),
-				Math.max(maxRadius, Math.min(viewHeight - maxRadius, y)),
+				Math.max(radius, Math.min(viewWidth - radius, x)),
+				Math.max(radius, Math.min(viewHeight - radius, y)),
 				radius);
 	}
 
