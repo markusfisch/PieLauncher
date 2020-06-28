94e1465cce8e0dae572e0c904a876bfa85b5b7d1
Markus Fisch <mf@markusfisch.de>
2020-06-28 15:49:04 +0200

    Keep the keyboard from repositioning the layout

--- app/src/main/java/de/markusfisch/android/pielauncher/activity/HomeActivity.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/activity/HomeActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowInsets;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
@@ -371,6 +372,8 @@ public class HomeActivity extends Activity {
 				Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
 			return;
 		}
+		window.setSoftInputMode(
+				WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 		window.getDecorView().setSystemUiVisibility(
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
 						View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
