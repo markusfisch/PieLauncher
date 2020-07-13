8e8c0691ac0b767559e692931e5bd89700ac07bd
Markus Fisch <mf@markusfisch.de>
2020-07-13 21:10:35 +0200

    Use LauncherApps.registerCallback() on Nougat
    Because ordinary package broadcasts mysteriously stop working after
    a while on Android Nougat.
    
    I don't know why, but I suppose it's because this app doesn't have
    a sticky service which seems to be required on Nougat.

--- app/src/main/java/de/markusfisch/android/pielauncher/app/PieLauncherApp.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/app/PieLauncherApp.java
@@ -1,8 +1,12 @@
 package de.markusfisch.android.pielauncher.app;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.LauncherApps;
+import android.os.Build;
+import android.os.UserHandle;
 
 import de.markusfisch.android.pielauncher.content.AppMenu;
 import de.markusfisch.android.pielauncher.preference.Preferences;
@@ -19,10 +23,18 @@ public class PieLauncherApp extends Application {
 	public void onCreate() {
 		super.onCreate();
 		prefs.init(this);
-		registerPackageEventReceiver();
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+				Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+			// because package broadcasts stop mysteriously working after
+			// a while on Android Nougat only
+			registerCallback();
+		} else {
+			registerPackageEventReceiver();
+		}
 	}
 
-	public void registerPackageEventReceiver() {
+	private void registerPackageEventReceiver() {
 		IntentFilter filter = new IntentFilter();
 		filter.addAction(Intent.ACTION_LOCALE_CHANGED);
 		filter.addAction(Intent.ACTION_PACKAGE_ADDED);
@@ -34,5 +46,40 @@ public class PieLauncherApp extends Application {
 		registerReceiver(packageEventReceiver, filter);
 		// Note it's not required to unregister the receiver because it
 		// needs to be there as long as this application is running.
+	}
+
+	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+	private void registerCallback() {
+		LauncherApps launcherApps = (LauncherApps) getSystemService(
+				LAUNCHER_APPS_SERVICE);
+		launcherApps.registerCallback(new LauncherApps.Callback() {
+			@Override
+			public void onPackageAdded(String packageName,
+					UserHandle user) {
+				appMenu.indexAppsAsync(PieLauncherApp.this, packageName);
+			}
+
+			@Override
+			public void onPackageChanged(String packageName,
+					UserHandle user) {
+				appMenu.indexAppsAsync(PieLauncherApp.this, packageName);
+			}
+
+			@Override
+			public void onPackageRemoved(String packageName,
+					UserHandle user) {
+				appMenu.removePackageAsync(packageName);
+			}
+
+			@Override
+			public void onPackagesAvailable(String[] packageNames,
+					UserHandle user, boolean replacing) {
+			}
+
+			@Override
+			public void onPackagesUnavailable(String[] packageNames,
+					UserHandle user, boolean replacing) {
+			}
+		});
 	}
 }
