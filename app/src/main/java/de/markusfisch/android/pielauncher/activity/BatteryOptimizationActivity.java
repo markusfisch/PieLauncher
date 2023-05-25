e8f32389cffa4371bcc8d8b81ef24911f1aee195
BrowseSky <134234586+BrowseSky@users.noreply.github.com>
2023-05-25 23:07:28 +0530

    Update BatteryOptimizationActivity.java
     No need 
    import android.view.View;

--- app/src/main/java/de/markusfisch/android/pielauncher/activity/BatteryOptimizationActivity.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/activity/BatteryOptimizationActivity.java
@@ -2,8 +2,6 @@ package de.markusfisch.android.pielauncher.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-
 import de.markusfisch.android.pielauncher.R;
 import de.markusfisch.android.pielauncher.os.BatteryOptimization;
 import de.markusfisch.android.pielauncher.view.SystemBars;
@@ -13,14 +11,11 @@ public class BatteryOptimizationActivity extends Activity {
 	protected void onCreate(Bundle state) {
 		super.onCreate(state);
 		setContentView(R.layout.activity_battery_optimization);
-
-		findViewById(R.id.disable_battery_optimization).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				BatteryOptimization.requestDisable(
-						BatteryOptimizationActivity.this);
-				finish();
-			}
+               
+		findViewById(R.id.disable_battery_optimization).setOnClickListener(v -> {
+			BatteryOptimization.requestDisable(
+					BatteryOptimizationActivity.this);
+			finish();
 		});
 
 		SystemBars.setTransparentSystemBars(getWindow());
