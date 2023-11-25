c6c70a5d4cfca0de6889279ab6ddd04702f7df98
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2023-11-25 16:53:41 +0100

    Add german translation

--- app/src/main/java/de/markusfisch/android/pielauncher/activity/SettingsActivity.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/activity/SettingsActivity.java
@@ -10,7 +10,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
