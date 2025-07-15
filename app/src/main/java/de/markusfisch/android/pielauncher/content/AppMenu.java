5ffefd0da6fb985eaa38bc6f316d95aa7c132299
Sergueille <sergueille.bismuth@gmail.com>
2025-07-15 09:19:43 +0200

    Fix launching apps when main activity is not recognized as enabled

--- app/src/main/java/de/markusfisch/android/pielauncher/content/AppMenu.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/content/AppMenu.java
@@ -606,12 +606,12 @@ public class AppMenu extends CanvasPieMenu {
 				ComponentName componentName = findEnabledActivity(
 						icon.componentName.getPackageName(),
 						icon.userHandle);
-				if (componentName == null) {
-					toast(context, R.string.activity_not_enabled);
-					return;
-				}
-				icon.componentName = componentName;
-			}
+                if (componentName != null) {
+                    icon.componentName = componentName;
+                }
+				// If no enabled activity is found, try to launch anyway
+				// with the disabled activity, just in case it works
+            }
 			launcherApps.startMainActivity(
 					icon.componentName,
 					icon.userHandle,
