a888815e443666210635c19dc7108217ad56e2a7
Markus Fisch <mf@markusfisch.de>
2025-11-17 19:58:03 +0100

    Only select pie icon when touch is at center
    Prevents premature selection.

--- app/src/main/java/de/markusfisch/android/pielauncher/graphics/PieMenu.java
+++ app/src/main/java/de/markusfisch/android/pielauncher/graphics/PieMenu.java
@@ -143,7 +143,7 @@ public class PieMenu<T extends PieMenu.Icon> {
 					}
 				}
 
-				if (!cursorNearCenter) {
+				if (!cursorNearCenter && cursorRadius > rad) {
 					selectedIcon = closestIcon;
 				}
 			}
